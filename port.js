2a003f5d55bbb1fe593d74538595dbf5705453c1
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2017-11-21 10:46:18 +1000

    fix: use bus.errors

--- port.js
+++ port.js
@@ -8,8 +8,8 @@ function Port(params) {
     this.log = {};
     this.logFactory = (params && params.logFactory) || null;
     this.bus = (params && params.bus) || null;
-    let defineError = this.defineError = (this.bus && this.bus.defineError) || params.defineError;
-    this.getError = (this.bus && this.bus.getError) || params.getError;
+    let defineError = this.defineError = (this.bus && this.bus.errors.defineError) || params.defineError;
+    this.getError = (this.bus && this.bus.errors.getError) || params.getError;
     let PortError = defineError('port');
     this.errors = {
         missingParams: defineError('missingParameters', PortError, 'Missing parameters'),
