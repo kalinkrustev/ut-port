7801d37e468c28239a7dd0097776e01a929644f9
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-06-16 08:54:48 +0300

    fix(utMeta): handle passed $meta [UTCORE-121]

--- meta.js
+++ meta.js
@@ -1,9 +1,10 @@
 const uuid = require('uuid').v4;
 module.exports = {
     utMeta: ($meta = {}) => ({
+        ...$meta,
         forward: {
+            ...$meta.forward,
             'x-b3-traceid': uuid().replace(/-/g, '')
-        },
-        ...$meta
+        }
     })
 };
